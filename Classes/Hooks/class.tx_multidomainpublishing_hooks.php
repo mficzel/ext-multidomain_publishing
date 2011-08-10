db6cf74a25b9f56676f299b6647d8e3c1ffbe293
Martin Ficzel <ficzel@work.de>
2011-08-10 13:57:09 +0000

    [FIX] renamed some hook methods
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/multidomain_publishing/trunk@50927 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Hooks/class.tx_multidomainpublishing_hooks.php
+++ Classes/Hooks/class.tx_multidomainpublishing_hooks.php
@@ -33,10 +33,10 @@ class tx_multidomainpublishing_hooks implements t3lib_Singleton, tslib_menu_filt
 	 * Get the pagetype from the Domain record
 	 * 
 	 * @param array $params Array of the Parameters
-	 * @param tslib_fe $tsfe calling Object
+	 * @param tslib_fe $tsfe calling TSFE Object
 	 * @return string extra SQL to add 
 	 */
-	public function getPagetypeFromDomain(array $params, tslib_fe $tsfe){
+	public function tslib_fe_checkAlternativeIdMethodsPostProcHook(array $params, tslib_fe $tsfe){
 		
 		$domainRecord = self::getDomainRecord();
 		
@@ -52,10 +52,10 @@ class tx_multidomainpublishing_hooks implements t3lib_Singleton, tslib_menu_filt
 	 * Show a 404 if a page is not allowed on the active domain
 	 * 
 	 * @param array $params Array of the given Parameter
-	 * @param tslib_fe $ref 
+	 * @param tslib_fe $tsfe calling TSFE Object 
 	 * @return void
 	 */
-	public function filterCurrentlyDisplayedPage(array $params, tslib_fe $tsfe){
+	public function tslib_fe_contentPostProcAllHook(array $params, tslib_fe $tsfe){
 		
 		if ( $visibility = $tsfe->page['tx_multidomainpublishing_visibility'] ){
 			if ( ! ($visibility == '' || $visibility == 0) ){
@@ -75,11 +75,11 @@ class tx_multidomainpublishing_hooks implements t3lib_Singleton, tslib_menu_filt
 	/**
 	 * Extend the Enable Field Method to make use of the 
 	 * 
-	 * @param array $params Array of the given Parameter
-	 * @param mixed $ref calling Object
+	 * @param array      $params Array of the given Parameter
+	 * @param t3lib_pageSelect $ref calling Object
 	 * @return string extra SQL to add 
 	 */
-	public function addDomainEnableFields( $params , $ref ){
+	public function t3lib_page_addEnableColumnsHook( array $params , t3lib_pageSelect $ref ){
 
 		if ( $params['ctrl']['tx_multidomainpublishing_column'] && $params['ctrl']['tx_multidomainpublishing_column'] != '' ){
 			$domainRecord = self::getDomainRecord();
@@ -101,8 +101,7 @@ class tx_multidomainpublishing_hooks implements t3lib_Singleton, tslib_menu_filt
 	 * @param	tslib_tmenu	The menu object
 	 * @return	boolean		Returns true if the page can be safely included.
 	 */
-	
-	public function tslib_menu_filterMenuPagesHook (array &$data, array $banUidArray, $spacer, tslib_menu $obj){
+	public function tslib_menu_filterMenuPagesHook (array &$data, array $banUidArray, $spacer, tslib_menu $ref){
 		
 		if ( $visibility = $data['tx_multidomainpublishing_visibility'] ){
 			if ($visibility == '' || $visibility == 0){
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,13 +1,13 @@
 <?php
 
 // get the pagetype from the domain if no explicit type is given
-$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['checkAlternativeIdMethods-PostProc']['multidomain_publishing'] = 'EXT:'.$_EXTKEY.'/Classes/Hooks/class.tx_multidomainpublishing_hooks.php:&tx_multidomainpublishing_hooks->getPagetypeFromDomain';
+$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['checkAlternativeIdMethods-PostProc']['multidomain_publishing'] = 'EXT:'.$_EXTKEY.'/Classes/Hooks/class.tx_multidomainpublishing_hooks.php:&tx_multidomainpublishing_hooks->tslib_fe_checkAlternativeIdMethodsPostProcHook';
 
 // filter pages which should not be displayed on the given domain
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all']['multidomain_publishing'] = 'EXT:'.$_EXTKEY.'/Classes/Hooks/class.tx_multidomainpublishing_hooks.php:&tx_multidomainpublishing_hooks->filterCurrentlyDisplayedPage';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all']['multidomain_publishing'] = 'EXT:'.$_EXTKEY.'/Classes/Hooks/class.tx_multidomainpublishing_hooks.php:&tx_multidomainpublishing_hooks->tslib_fe_contentPostProcAllHook';
 
 // add a hook for the enable fields which adds clauses for the tables 
-$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_page.php']['addEnableColumns']['multidomain_publishing'] = 'EXT:'.$_EXTKEY.'/Classes/Hooks/class.tx_multidomainpublishing_hooks.php:&tx_multidomainpublishing_hooks->addDomainEnableFields';
+$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_page.php']['addEnableColumns']['multidomain_publishing'] = 'EXT:'.$_EXTKEY.'/Classes/Hooks/class.tx_multidomainpublishing_hooks.php:&tx_multidomainpublishing_hooks->t3lib_page_addEnableColumnsHook';
 
 // add a hook to filter pages from the menus 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/tslib/class.tslib_menu.php']['filterMenuPages']['multidomain_publishing'] = 'EXT:'.$_EXTKEY.'/Classes/Hooks/class.tx_multidomainpublishing_hooks.php:tx_multidomainpublishing_hooks';
