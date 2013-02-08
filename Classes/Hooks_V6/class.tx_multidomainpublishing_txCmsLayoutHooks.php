e728cd6528ba93b7c00648e1f834319007403a1b
Martin Ficzel <ficzel@work.de>
2013-02-08 10:21:01 +0100

    [WIP] added separate hook implementations for TYPO3 6.x that use the namespaces properly

---
+++ Classes/Hooks_V6/class.tx_multidomainpublishing_txCmsLayoutHooks.php
@@ -0,0 +1,85 @@
+<?php
+/***************************************************************
+* Copyright notice
+*
+* (c) 2009 by n@work GmbH
+*
+* All rights reserved
+*
+* This script is part of the Multidomain Publishing extension. The
+* Multidomain Publishing extension is free software; you can redistribute
+* it and/or modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2 of the
+* License, or (at your option) any later version.
+*
+* The GNU General Public License can be found at
+* http://www.gnu.org/copyleft/gpl.html.
+*
+* This script is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * This is a file of the multidomain_publishing project.
+ * http://forge.typo3.org/projects/extension-multidomain_publishing
+ *
+ * Project sponsored by:
+ * n@work GmbH - http://www.work.de
+ *
+ * $Id$
+ */
+
+require_once (t3lib_extMgm::extPath('multidomain_publishing')  . 'Classes/class.tx_multidomainpublishing_constants.php');
+require_once (t3lib_extMgm::extPath('multidomain_publishing')  . 'Classes/Helpers/class.tx_multidomainpublishing_domainHelper.php');
+require_once (t3lib_extMgm::extPath('cms')  . 'layout/interfaces/interface.tx_cms_layout_tt_content_drawitemhook.php');
+
+/**
+ * Implement mutidomain hooks for tx_cms_layout
+ *
+ * @author Martin Ficzel <martin@work.de>
+ * @author Thomas Hempel <thomas@work.de>
+ *
+ * @package TYPO3
+ * @subpackage multidomain_publishing
+ */
+class tx_multidomainpublishing_txCmsLayoutHooks implements \TYPO3\CMS\Core\SingletonInterface, \TYPO3\CMS\Backend\View\PageLayoutViewDrawItemHookInterface {
+
+	/**
+	 * Preprocesses the preview rendering of a content element.
+	 *
+	 * @param	\TYPO3\CMS\Backend\View\PageLayoutView		$parentObject: Calling parent object
+	 * @param	boolean				$drawItem: Whether to draw the item using the default functionalities
+	 * @param	string				$headerContent: Header content
+	 * @param	string				$itemContent: Item content
+	 * @param	array				$row: Record row of tt_content
+	 * @return	void
+	 */
+	public function preProcess( \TYPO3\CMS\Backend\View\PageLayoutView &$parentObject, &$drawItem, &$headerContent, &$itemContent, array &$row){
+		if ($row['tx_multidomainpublishing_visibility'] && $row['tx_multidomainpublishing_visibility'] != 0 ){
+
+			$domainIds = explode(',', $row['tx_multidomainpublishing_visibility']);
+			$domainInfos = array();
+			foreach ($domainIds as $domainId) {
+				$domain = tx_multidomainpublishing_domainHelper::getDomainRecordById($domainId);
+				if ($domain) {
+					if ($domain['tx_multidomainpublishing_mode'] == 0) {
+						$modeInfo = $GLOBALS['LANG']->sL('LLL:EXT:multidomain_publishing/locallang_db.xml:sys_domain.tx_multidomainpublishing_mode_deny_label') . ' ';
+					} else {
+						$modeInfo = $GLOBALS['LANG']->sL('LLL:EXT:multidomain_publishing/locallang_db.xml:sys_domain.tx_multidomainpublishing_mode_allow_label') . ' ';
+					}
+
+					$domainInfos[] = $modeInfo . ' ' . $domain['domainName'];
+				}
+			}
+
+			if (count($domainInfos) > 0) {
+				$itemContent .= '<strong>' . $GLOBALS['LANG']->sL('LLL:EXT:multidomain_publishing/locallang_db.xml:pages.tx_multidomainpublishing_visibility') . ':</strong><ul><li>' . implode('</li><li>', $domainInfos) . '</li></ul>';
+			}
+		}
+	}
+}
+?>
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,18 +1,39 @@
 <?php
 
-// get the pagetype from the domain if no explicit type is given
-$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['checkAlternativeIdMethods-PostProc']['multidomain_publishing'] = 'EXT:'.$_EXTKEY.'/Classes/Hooks/class.tx_multidomainpublishing_tslibFeHooks.php:&tx_multidomainpublishing_tslibFeHooks->checkAlternativeIdMethodsPostProcHook';
+$typo3VersionAsInteger = 0;
 
-// filter pages which should not be displayed on the given domain
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all']['multidomain_publishing'] = 'EXT:'.$_EXTKEY.'/Classes/Hooks/class.tx_multidomainpublishing_tslibFeHooks.php:&tx_multidomainpublishing_tslibFeHooks->contentPostProcAllHook';
+// try all available classes to get the version int
+if (class_exists('\TYPO3\CMS\Core\Utility\VersionNumberUtility')) {
+	$typo3VersionAsInteger = \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version);
+} else if (class_exists('t3lib_utility_VersionNumber')) {
+	$typo3VersionAsInteger = t3lib_utility_VersionNumber::convertVersionNumberToInteger(TYPO3_version);
+} else if (class_exists('t3lib_div')) {
+	$typo3VersionAsInteger = t3lib_div::int_from_ver(TYPO3_version);
+}
 
-// add a hook for the enable fields which adds clauses for the tables 
-$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_page.php']['addEnableColumns']['multidomain_publishing'] = 'EXT:'.$_EXTKEY.'/Classes/Hooks/class.tx_multidomainpublishing_t3libPageHooks.php:&tx_multidomainpublishing_t3libPageHooks->addEnableColumnsHook';
-
-
-// add a hook to filter pages from the menus 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/tslib/class.tslib_menu.php']['filterMenuPages']['multidomain_publishing'] = 'EXT:'.$_EXTKEY.'/Classes/Hooks/class.tx_multidomainpublishing_tslibMenuHooks.php:tx_multidomainpublishing_tslibMenuHooks';
-
-// add hook to show domain constraints in page_module
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['multidomain_publishing'] = 'EXT:'.$_EXTKEY.'/Classes/Hooks/class.tx_multidomainpublishing_txCmsLayoutHooks.php:tx_multidomainpublishing_txCmsLayoutHooks';
+if ($typo3VersionAsInteger >= 6000000) {
+	// Code for 6.x
+	// get the pagetype from the domain if no explicit type is given
+	$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['checkAlternativeIdMethods-PostProc'][$_EXTKEY] = 'EXT:'.$_EXTKEY.'/Classes/Hooks_V6/class.tx_multidomainpublishing_tslibFeHooks.php:&tx_multidomainpublishing_tslibFeHooks->checkAlternativeIdMethodsPostProcHook';
+	// filter pages which should not be displayed on the given domain
+	$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'][$_EXTKEY] = 'EXT:'.$_EXTKEY.'/Classes/Hooks_V6/class.tx_multidomainpublishing_tslibFeHooks.php:&tx_multidomainpublishing_tslibFeHooks->contentPostProcAllHook';
+	// add a hook for the enable fields which adds clauses for the tables
+	$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_page.php']['addEnableColumns'][$_EXTKEY] = 'EXT:'.$_EXTKEY.'/Classes/Hooks_V6/class.tx_multidomainpublishing_t3libPageHooks.php:&tx_multidomainpublishing_t3libPageHooks->addEnableColumnsHook';
+	// add a hook to filter pages from the menus
+	$TYPO3_CONF_VARS['SC_OPTIONS']['cms/tslib/class.tslib_menu.php']['filterMenuPages'][$_EXTKEY] = 'EXT:'.$_EXTKEY.'/Classes/Hooks_V6/class.tx_multidomainpublishing_tslibMenuHooks.php:tx_multidomainpublishing_tslibMenuHooks';
+	// add hook to show domain constraints in page_module
+	$TYPO3_CONF_VARS['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem'][$_EXTKEY] = 'EXT:'.$_EXTKEY.'/Classes/Hooks_V6/class.tx_multidomainpublishing_txCmsLayoutHooks.php:tx_multidomainpublishing_txCmsLayoutHooks';
+} else {
+	// Code up to 4.x
+	// get the pagetype from the domain if no explicit type is given
+	$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['checkAlternativeIdMethods-PostProc'][$_EXTKEY] = 'EXT:'.$_EXTKEY.'/Classes/Hooks_V4/class.tx_multidomainpublishing_tslibFeHooks.php:&tx_multidomainpublishing_tslibFeHooks->checkAlternativeIdMethodsPostProcHook';
+	// filter pages which should not be displayed on the given domain
+	$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'][$_EXTKEY] = 'EXT:'.$_EXTKEY.'/Classes/Hooks_V4/class.tx_multidomainpublishing_tslibFeHooks.php:&tx_multidomainpublishing_tslibFeHooks->contentPostProcAllHook';
+	// add a hook for the enable fields which adds clauses for the tables
+	$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_page.php']['addEnableColumns'][$_EXTKEY] = 'EXT:'.$_EXTKEY.'/Classes/Hooks_V4/class.tx_multidomainpublishing_t3libPageHooks.php:&tx_multidomainpublishing_t3libPageHooks->addEnableColumnsHook';
+	// add a hook to filter pages from the menus
+	$TYPO3_CONF_VARS['SC_OPTIONS']['cms/tslib/class.tslib_menu.php']['filterMenuPages'][$_EXTKEY] = 'EXT:'.$_EXTKEY.'/Classes/Hooks_V4/class.tx_multidomainpublishing_tslibMenuHooks.php:tx_multidomainpublishing_tslibMenuHooks';
+	// add hook to show domain constraints in page_module
+	$TYPO3_CONF_VARS['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem'][$_EXTKEY] = 'EXT:'.$_EXTKEY.'/Classes/Hooks_V4/class.tx_multidomainpublishing_txCmsLayoutHooks.php:tx_multidomainpublishing_txCmsLayoutHooks';
+}
 ?>
